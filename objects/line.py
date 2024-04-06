222b9afd26936eba0801adad9f5ba152bf9b50e4
sekulas <sekula.sebastian22@gmail.com>
2024-04-06 17:14:43 +0200

    perf: clipping faster version

--- objects/line.py
+++ objects/line.py
@@ -6,16 +6,16 @@ class Line:
         self.b = b
 
     def clip(self):
-        if self.a[2,0] * self.b[2,0] < 0:
-            dx = self.b[0,0] - self.a[0,0]
-            dy = self.b[1,0] - self.a[1,0]
-            dz = self.b[2,0] - self.a[2,0]
-            y = np.float16(-dy * (self.a[2,0] - 1)) / dz + self.a[1,0]
-            x = np.float16(-dx * (self.a[2,0] - 1)) / dz + self.a[0,0]
+        if np.sign(self.a[2,0]) != np.sign(self.b[2,0]):
+            #dx = self.b[0,0] - self.a[0,0]
+            #dy = self.b[1,0] - self.a[1,0]
+            #dz = self.b[2,0] - self.a[2,0]
+            #y = np.float16(-dy * (self.a[2,0] - 1)) / dz + self.a[1,0]
+            #x = np.float16(-dx * (self.a[2,0] - 1)) / dz + self.a[0,0]
             if self.a[2,0] < 0:
-                return Line(np.matrix([[x], [y], [0]]), self.b)
+                return Line(np.matrix([[self.a[0,0]], [self.a[1,0]], [0]]), self.b)
             else:
-                return Line(self.a, np.matrix([[x], [y], [0]]))
+                return Line(self.a, np.matrix([[self.b[0,0]], [self.b[1,0]], [0]]))
         return self
 
     def project_3d_to_2d(self, focal_len):
